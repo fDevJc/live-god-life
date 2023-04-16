44ac8f632f210ee54ca26376e306d0a4ed0f4414
fDevJc <fdevjc@gmail.com>
2023-04-16 21:26:29 +0900

    chore: 단일프로젝트 멀티모듈 초기 세팅

---
+++ api-modules/auth-api/src/main/java/com/godlife/authservice/domain/request/RequestLogin.java
@@ -0,0 +1,31 @@
+package com.godlife.authservice.domain.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RequestLogin {
+    /**
+     * 회원 로그인 타입
+     */
+    private String type;
+
+    /**
+     * 회원 로그인 식별 값
+     */
+    private String identifier;
+
+    /**
+     * RequestLogin 객체 생성 정적 팩토리 메소드
+     * @param type          로그인 타입
+     * @param identifier    로그인 식별자
+     * @return RequestLogin
+     */
+    public static RequestLogin of(String type, String identifier) {
+        return new RequestLogin(type, identifier);
+    }
+}
