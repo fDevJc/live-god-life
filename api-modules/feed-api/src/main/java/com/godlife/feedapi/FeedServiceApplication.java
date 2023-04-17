1f0f3d2f8b2b04143c0613938627884191f7ecd4
fDevJc <fdevjc@gmail.com>
2023-04-17 22:19:57 +0900

    refactor: 피드api 엔티티, 레파지토리 스캔 설정 추가

--- api-modules/feed-api/src/main/java/com/godlife/feedapi/FeedServiceApplication.java
+++ api-modules/feed-api/src/main/java/com/godlife/feedapi/FeedServiceApplication.java
@@ -2,11 +2,16 @@ package com.godlife.feedapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
-// @SpringBootApplication(scanBasePackages = {"com.godlife.feedapi", "com.godlife.feeddomain"})
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.godlife.feeddomain"})
+@EntityScan(basePackages = {"com.godlife.feeddomain"})
+@SpringBootApplication(
+	scanBasePackages = {"com.godlife.feedapi", "com.godlife.feeddomain"}
+)
 public class FeedServiceApplication {
 
 	public static void main(String[] args) {
@@ -14,3 +19,4 @@ public class FeedServiceApplication {
 	}
 
 }
+
