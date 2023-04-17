0a430ce284ce22acf2eccf5d4b2c1a5180886975
fDevJc <fdevjc@gmail.com>
2023-04-18 00:04:11 +0900

    refactor: yml 리팩토링

--- domain-modules/feed-domain/src/main/java/com/godlife/feeddomain/service/FeedService.java
+++ domain-modules/feed-domain/src/main/java/com/godlife/feeddomain/service/FeedService.java
@@ -32,75 +32,75 @@ public class FeedService {
 	private final MindsetRepository mindsetRepository;
 	private final TodoRepository todoRepository;
 
-	private final UserServiceClient userServiceClient;
+	// private final UserServiceClient userServiceClient;
 
 	public List<FeedsDto> getFeeds(Pageable page, Long userId, @Nullable String category, @Nullable List<Long> feedIds) {
 
 		List<FeedsDto> feedsDtos = feedRepository.findAllByCategoryAndFeedIds(page, category, feedIds);
-		feedDtosSetUserInfo(feedsDtos, getUsersInfoUsingAPI(getUserIdsToString(feedsDtos)));
-		feedDtosSetBookmarkInfo(feedsDtos, getBookmarksInfoUsingAPI(userId, getFeedIdsToString(feedsDtos)));
+		// feedDtosSetUserInfo(feedsDtos, getUsersInfoUsingAPI(getUserIdsToString(feedsDtos)));
+		// feedDtosSetBookmarkInfo(feedsDtos, getBookmarksInfoUsingAPI(userId, getFeedIdsToString(feedsDtos)));
 		return feedsDtos;
 	}
-
-	private void feedDtosSetBookmarkInfo(List<FeedsDto> feedsDtos, List<BookmarkResponse.BookmarkDto> bookmarkDtos) {
-
-		bookmarkDtos.forEach(bookmarkDto -> feedsDtos.stream()
-			.filter(feedsDto -> feedsDto.getFeedId().equals(bookmarkDto.getFeedId()))
-			.findFirst()
-			.orElseThrow()
-			.registerBookmark(bookmarkDto.isBookmarkStatus()));
-	}
-
-	private void feedDtosSetUserInfo(List<FeedsDto> feedsDtos, List<UserResponse.UserDto> userDtos) {
-
-		userDtos.forEach(userDto -> feedsDtos.stream()
-			.filter(feedsDto -> feedsDto.getUserId().equals(userDto.getUserId()))
-			.collect(Collectors.toList())
-			.forEach(feedsDto -> feedsDto.registerUser(userDto)));
-	}
-
-	private List<UserResponse.UserDto> getUsersInfoUsingAPI(String ids) {
-		try {
-			return userServiceClient.getUsers(ids).getData();
-		} catch (Exception e) {
-			log.error(e.getMessage());
-			return List.of();
-		}
-	}
-
-	private static String getUserIdsToString(List<FeedsDto> feeds) {
-		String ids = feeds.stream()
-			.map(feed -> feed.getUserId().toString())
-			.collect(Collectors.joining(","));
-		return ids;
-	}
-
-	private List<BookmarkResponse.BookmarkDto> getBookmarksInfoUsingAPI(Long userId, String ids) {
-		try {
-			return userServiceClient.getBookmarks(userId, ids).getData();
-		} catch (Exception e) {
-			log.error(e.getMessage());
-			return List.of();
-		}
-	}
-
-	private static String getFeedIdsToString(List<FeedsDto> feeds) {
-		return feeds.stream()
-			.map(feed -> feed.getFeedId().toString())
-			.collect(Collectors.joining(","));
-	}
+	//
+	// private void feedDtosSetBookmarkInfo(List<FeedsDto> feedsDtos, List<BookmarkResponse.BookmarkDto> bookmarkDtos) {
+	//
+	// 	bookmarkDtos.forEach(bookmarkDto -> feedsDtos.stream()
+	// 		.filter(feedsDto -> feedsDto.getFeedId().equals(bookmarkDto.getFeedId()))
+	// 		.findFirst()
+	// 		.orElseThrow()
+	// 		.registerBookmark(bookmarkDto.isBookmarkStatus()));
+	// }
+	//
+	// private void feedDtosSetUserInfo(List<FeedsDto> feedsDtos, List<UserResponse.UserDto> userDtos) {
+	//
+	// 	userDtos.forEach(userDto -> feedsDtos.stream()
+	// 		.filter(feedsDto -> feedsDto.getUserId().equals(userDto.getUserId()))
+	// 		.collect(Collectors.toList())
+	// 		.forEach(feedsDto -> feedsDto.registerUser(userDto)));
+	// }
+	//
+	// private List<UserResponse.UserDto> getUsersInfoUsingAPI(String ids) {
+	// 	try {
+	// 		return userServiceClient.getUsers(ids).getData();
+	// 	} catch (Exception e) {
+	// 		log.error(e.getMessage());
+	// 		return List.of();
+	// 	}
+	// }
+	//
+	// private static String getUserIdsToString(List<FeedsDto> feeds) {
+	// 	String ids = feeds.stream()
+	// 		.map(feed -> feed.getUserId().toString())
+	// 		.collect(Collectors.joining(","));
+	// 	return ids;
+	// }
+	//
+	// private List<BookmarkResponse.BookmarkDto> getBookmarksInfoUsingAPI(Long userId, String ids) {
+	// 	try {
+	// 		return userServiceClient.getBookmarks(userId, ids).getData();
+	// 	} catch (Exception e) {
+	// 		log.error(e.getMessage());
+	// 		return List.of();
+	// 	}
+	// }
+	//
+	// private static String getFeedIdsToString(List<FeedsDto> feeds) {
+	// 	return feeds.stream()
+	// 		.map(feed -> feed.getFeedId().toString())
+	// 		.collect(Collectors.joining(","));
+	// }
 
 	@Transactional
 	public FeedMindsetsTodosDto getFeedDetail(Long userId, Long feedId) {
 		FeedMindsetsTodosDto feedMindsetsTodosDto = feedRepository.findFeedWithMindsetsAndTodosByFeedId(feedId);
-		feedMindsetsTodosDto.setUserInfo(getUsersInfoUsingAPI(feedMindsetsTodosDto.getUserId().toString()));
-		feedMindsetsTodosDto.setBookmarkStatus(
-			getBookmarksInfoUsingAPI(userId, feedId.toString())
-				.stream()
-				.filter(bookmarkDto -> bookmarkDto.getFeedId().equals(feedId))
-				.findAny()
-				.orElseThrow(() -> new NoSuchBookmarkException(feedId))
-				.isBookmarkStatus());
+		// feedMindsetsTodosDto.setUserInfo(getUsersInfoUsingAPI(feedMindsetsTodosDto.getUserId().toString()));
+		// feedMindsetsTodosDto.setBookmarkStatus(
+		// 	getBookmarksInfoUsingAPI(userId, feedId.toString())
+		// 		.stream()
+		// 		.filter(bookmarkDto -> bookmarkDto.getFeedId().equals(feedId))
+		// 		.findAny()
+		// 		.orElseThrow(() -> new NoSuchBookmarkException(feedId))
+		// 		.isBookmarkStatus());
 		return feedMindsetsTodosDto;
 	}
 
