f3afb7be8593d77993f7443674d73fb45f0e9de0
fDevJc <fdevjc@gmail.com>
2023-04-18 18:19:19 +0900

    refactor: RequestBuilder 생성 및 중복 테스트 코드 제거

--- api-modules/feed-api/src/test/java/com/godlife/feedapi/acceptance/FeedAcceptanceTest.java
+++ api-modules/feed-api/src/test/java/com/godlife/feedapi/acceptance/FeedAcceptanceTest.java
@@ -1,7 +1,5 @@
 package com.godlife.feedapi.acceptance;
 
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,12 +7,10 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import com.godlife.feedapi.presentation.dto.request.CreateFeedRequest;
-import com.godlife.feeddomain.domain.enums.Category;
-import com.godlife.feeddomain.domain.enums.RepetitionType;
-import com.godlife.feeddomain.domain.enums.TodoType;
+import com.godlife.feedapi.common.RequestBuilder;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FeedAcceptanceTest {
@@ -27,93 +23,29 @@ public class FeedAcceptanceTest {
 	}
 
 	@Test
-	void 피드_등록_요청() throws Exception {
-		//given
-		CreateFeedRequest.CreateFeedContentRequest createFeedContentRequest = CreateFeedRequest.CreateFeedContentRequest.builder()
-			.title("content-title")
-			.content("content-content")
-			.orderNumber(0)
-			.build();
-
-		CreateFeedRequest.CreateFeedMindsetRequest createFeedMindsetRequest = CreateFeedRequest.CreateFeedMindsetRequest.builder()
-			.content("mindset-content")
-			.build();
-
-		CreateFeedRequest.CreateFeedTodoRequest createFeedTodoRequest = CreateFeedRequest.CreateFeedTodoRequest.builder()
-			.title("toto-task")
-			.depth(1)
-			.notification("0900")
-			.orderNumber(0)
-			.type(TodoType.TASK.name())
-			.period(90)
-			.repetitionType(RepetitionType.DAY.name())
-			.build();
-
-		CreateFeedRequest createFeedRequest = CreateFeedRequest.builder()
-			.categoryCode(Category.CAREER.name())
-			.image("image")
-			.title("feed-title")
-			.todos(List.of(createFeedTodoRequest))
-			.contents(List.of(createFeedContentRequest))
-			.mindsets(List.of(createFeedMindsetRequest))
-			.userId(1L)
-			.build();
-
-		RestAssured
-			.given().log().all()
-			.contentType(MediaType.APPLICATION_JSON_VALUE)
-			.accept(MediaType.APPLICATION_JSON_VALUE)
-			.header("x-user", 1L)
-			.body(createFeedRequest)
-			.when()
-			.post("/feeds")
+	void 사용자는_피드를_등록할_수_있다() throws Exception {
+		피드_등록_요청()
 			.then().log().all()
 			.statusCode(HttpStatus.CREATED.value());
 	}
 
-	@Test
-	void 피드_조회_요청() throws Exception {
-		CreateFeedRequest.CreateFeedContentRequest createFeedContentRequest = CreateFeedRequest.CreateFeedContentRequest.builder()
-			.title("content-title")
-			.content("content-content")
-			.orderNumber(0)
-			.build();
-
-		CreateFeedRequest.CreateFeedMindsetRequest createFeedMindsetRequest = CreateFeedRequest.CreateFeedMindsetRequest.builder()
-			.content("mindset-content")
-			.build();
-
-		CreateFeedRequest.CreateFeedTodoRequest createFeedTodoRequest = CreateFeedRequest.CreateFeedTodoRequest.builder()
-			.title("toto-task")
-			.depth(1)
-			.notification("0900")
-			.orderNumber(0)
-			.type(TodoType.TASK.name())
-			.period(90)
-			.repetitionType(RepetitionType.DAY.name())
-			.build();
-
-		CreateFeedRequest createFeedRequest = CreateFeedRequest.builder()
-			.categoryCode(Category.CAREER.name())
-			.image("image")
-			.title("feed-title")
-			.todos(List.of(createFeedTodoRequest))
-			.contents(List.of(createFeedContentRequest))
-			.mindsets(List.of(createFeedMindsetRequest))
-			.userId(1L)
-			.build();
-
-		RestAssured
+	private static Response 피드_등록_요청() {
+		return RestAssured
 			.given().log().all()
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.accept(MediaType.APPLICATION_JSON_VALUE)
 			.header("x-user", 1L)
-			.body(createFeedRequest)
+			.body(RequestBuilder.getCreateFeedRequest())
 			.when()
-			.post("/feeds")
-			.then().log().all()
-			.statusCode(HttpStatus.CREATED.value());
+			.post("/feeds");
+	}
 
+	@Test
+	void 피드_조회_요청() throws Exception {
+		//given
+		피드_등록_요청();
+
+		//when & then
 		RestAssured
 			.given().log().all()
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -127,10 +59,18 @@ public class FeedAcceptanceTest {
 
 	@Test
 	void 피드_상세_조회_요청() throws Exception {
-	    //given
+		//given
+		피드_등록_요청();
 
-	    //when
-
-	    //then
+		//when & then
+		RestAssured
+			.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.header("x-user", 1L)
+			.when()
+			.get("/feeds/1")
+			.then().log().all()
+			.statusCode(HttpStatus.OK.value());
 	}
 }
