05fc3c0e74c16c5a93ad43198d5ca1c6fe2ec181
fDevJc <fdevjc@gmail.com>
2023-04-19 16:02:00 +0900

    test: 사용자 목표조회 통합테스트

--- api-modules/goal-api/src/test/java/com/godlife/goalapi/acceptance/GoalAcceptanceTest.java
+++ api-modules/goal-api/src/test/java/com/godlife/goalapi/acceptance/GoalAcceptanceTest.java
@@ -11,9 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestExecutionListeners;
 
 import com.godlife.goalapi.utils.CreateGoalRequestFactory;
-import com.godlife.goaldomain.dto.request.CreateGoalRequest;
 
 import io.restassured.RestAssured;
+import io.restassured.mapper.TypeRef;
+import io.restassured.response.Response;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @TestExecutionListeners(
@@ -30,19 +31,42 @@ public class GoalAcceptanceTest {
 		RestAssured.port = port;
 	}
 
+	private final Long TEST_USER_ID = 1L;
+
 	@Test
 	void 사용자는_목표를_저장할_수_있다() throws Exception {
-		CreateGoalRequest createGoalRequest = CreateGoalRequestFactory.getCreateGoalRequest();
+		목표_저장_요청()
+			.then().log().all()
+			.statusCode(HttpStatus.CREATED.value());
+	}
 
-		RestAssured
+	private Response 목표_저장_요청() {
+		return RestAssured
 			.given().log().all()
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.accept(MediaType.APPLICATION_JSON_VALUE)
-			.header("x-user", 1L)
-			.body(createGoalRequest)
+			.header("x-user", TEST_USER_ID)
+			.body(CreateGoalRequestFactory.getCreateGoalRequest())
 			.when()
-			.post("/goals")
+			.post("/goals");
+	}
+
+	@Test
+	void 사용자는_자신의_모든_목표를_조회할_수_있다() throws Exception {
+		목표_저장_요청();
+		//ApiResponse<List<GoalDto>> ret =
+		RestAssured
+			.given()
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.header("x-user", TEST_USER_ID)
+			.when()
+			.get("/goals")
 			.then().log().all()
-			.statusCode(HttpStatus.CREATED.value());
+			.statusCode(HttpStatus.OK.value())
+			.extract()
+			.as(new TypeRef<>() {
+			});
+
+		// Assertions.assertThat(ret.getData().size()).isEqualTo(1);
 	}
 }
